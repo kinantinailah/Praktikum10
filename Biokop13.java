69ba1e2bf955f36b97ef76b6453fcc4fc016c477
kinantinailah <kinanti.nailah06@gmail.com>
2025-11-19 09:37:24 +0700

    pertanyaan no 5

--- Biokop13.java
+++ Biokop13.java
@@ -17,6 +17,10 @@ public class Biokop13 {
       System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
       System.out.println(penonton.length);
+
+      for (int i = 0; i < penonton.length; i++) {
+        System.out.println("Panjang baris ke-" + i + " : " + penonton[i].length);
+      }
       System.out.println(penonton[0].length);
       System.out.println(penonton[1].length);
       System.out.println(penonton[2].length);
--- TugasA.java
+++ TugasA.java
@@ -1,4 +1,4 @@
-public class Tugas {
+public class TugasA {
     public static void main(String[] args) {
         
     }
