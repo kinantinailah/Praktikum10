96de6c817b9e7bd058e090ff0378324085fa983d
kinantinailah <kinanti.nailah06@gmail.com>
2025-11-19 10:35:30 +0700

    pertanyaan 9

--- Biokop13.java
+++ Biokop13.java
@@ -18,8 +18,8 @@ public class Biokop13 {
 
       System.out.println("Penonton pada baris ke-3: ");
 
-      for (String i : penonton[2]) {
-          System.out.println(i);
+      for (int i = 0; i < penonton[2].length; i++ ) {
+          System.out.println("Penonton pada baris ke-" + (i+1) + ": " + penonton[i]);
          
         System.out.println("Jumlah penonton pada baris ini: " + barisPenonton.length);
       }
